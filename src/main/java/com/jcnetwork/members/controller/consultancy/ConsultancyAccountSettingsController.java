984606e1bd1cb53e963869ebcbdd729212250ca7
Eric Amann <eric-amann@outlook.de>
2021-08-23 04:06:56 +0200

    Added consultancy settings page

--- src/main/java/com/jcnetwork/members/controller/consultancy/ConsultancyAccountSettingsController.java
+++ src/main/java/com/jcnetwork/members/controller/consultancy/ConsultancyAccountSettingsController.java
@@ -1,13 +1,23 @@
 package com.jcnetwork.members.controller.consultancy;
 
+import com.jcnetwork.members.model.data.consultancy.Consultancy;
+import com.jcnetwork.members.model.data.consultancy.ConsultancyDetails;
+import com.jcnetwork.members.model.data.user.UserDetails;
+import com.jcnetwork.members.model.ui.Toast;
+import com.jcnetwork.members.security.model.User;
+import com.jcnetwork.members.service.ConsultancyService;
 import com.jcnetwork.members.utils.ControllerUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/{consultancy}/admin")
@@ -15,6 +25,9 @@ public class ConsultancyAccountSettingsController {
 
     @Autowired
     private ControllerUtils utils;
+
+    @Autowired
+    private ConsultancyService consultancyService;
 
     private final String PRIVILEGE_NAME = "ACCOUNT_SETTINGS";
 
@@ -28,10 +41,53 @@ public class ConsultancyAccountSettingsController {
                     "Vereinseinstellungen",
                     PRIVILEGE_NAME
             );
+            modelAndView.addObject("consultancyDetails", consultancyService.getByName(consultancyName).get().getConsultancyDetails());
             modelAndView.setViewName("sites/consultancy/admin/accountSettings");
             return modelAndView;
         } catch (Exception e) {
             return new ModelAndView(new RedirectView("/accessForbidden"));
         }
+    }
+
+    @PostMapping("/updateIcon")
+    public RedirectView updateIcon(
+            @PathVariable("consultancy") String consultancyName,
+            @Valid ConsultancyDetails newConsultancyDetails,
+            RedirectAttributes redirectAttributes
+    ) {
+        Consultancy consultancy = consultancyService.getByName(consultancyName).get();
+        ConsultancyDetails consultancyDetails = consultancy.getConsultancyDetails();
+        consultancyDetails.setIconBase64(newConsultancyDetails.getIconBase64());
+        consultancy.setConsultancyDetails(consultancyDetails);
+        consultancyService.save(consultancy);
+
+        Toast toast = new Toast(
+                "Speichern Erfolgreich",
+                "Das Vereinsicon wurde aktualisiert.",
+                "success"
+        );
+        redirectAttributes.addFlashAttribute("toast", toast);
+
+        return new RedirectView("/" + consultancyName + "/admin/accountSettings");
+    }
+
+    @PostMapping("/updateDetails")
+    public RedirectView updateDetails(
+            @PathVariable("consultancy") String consultancyName,
+            @Valid ConsultancyDetails consultancyDetails,
+            RedirectAttributes redirectAttributes
+    ) {
+        Consultancy consultancy = consultancyService.getByName(consultancyName).get();
+        consultancy.setConsultancyDetails(consultancyDetails);
+        consultancyService.save(consultancy);
+
+        Toast toast = new Toast(
+                "Speichern Erfolgreich",
+                "Die Vereinsdetails wurden aktualisiert.",
+                "success"
+        );
+        redirectAttributes.addFlashAttribute("toast", toast);
+
+        return new RedirectView("/" + consultancyName + "/admin/accountSettings");
     }
 }
